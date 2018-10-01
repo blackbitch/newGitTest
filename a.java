9cb40bfbf0353e8fea7a26c5a52e8d6d36176da0
lordozb <aadityaja@gmail.com>
2018-10-01 12:54:39 +0530

    #3

--- a.java
+++ a.java
@@ -3,5 +3,12 @@ class a{
 		System.out.pritnln("Hello world");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		String soln = "";
+		for(int i = 0; i < str.length(); i++){
+			char c = str.charAt(i);
+			if(Character.isLowerCase(c))
+				soln = soln+""+Character.toUpperCase(c);
+			else
+				soln = soln+""+Character.toLowerCase(c);
 	}
 }
